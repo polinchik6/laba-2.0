b86bd9b4b5ad7b63635482b781cf726b1f6efe48
polinchik6 <polinawey2@gmail.com>
2024-02-13 18:55:42 +0300

    add2

---
+++ CMake/лабораторная 2.cpp
@@ -0,0 +1,14 @@
+﻿// лабораторная 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	int i;
+	std::cin >> i;
+	unsigned char x = 7;
+	bool n = char(1 * pow(2, i)) & x;
+	std::cout << n << std::endl;
+}
+
